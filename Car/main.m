943ce978bc52d49c5a1803866a4bdf2a6e25177b
thodep <thodep@yahoo.com>
2015-04-09 16:40:32 -0400

    Dynamic Typing

--- Car/main.m
+++ Car/main.m
@@ -25,8 +25,44 @@ int main(int argc, const char * argv[]) {
         [toyota drive];
         
 
+        // Instantiating objects
+        Car *nissan = [[Car alloc] init];
+        NSLog(@"Created a %@", [nissan model]);
         
+        Car *chevy = [[Car alloc] initWithModel:@"Chevy Corvette"];
+        NSLog(@"Created a %@, too.", chevy.model);
 
+        Car *delorean = [[Car alloc] initWithModel:@"DeLorean"];
+        
+        // Get the class of an object
+        NSLog(@"%@ is an instance of the %@ class",
+              [delorean model], [delorean class]);
+        
+        // Check an object against a class and all subclasses
+        if ([delorean isKindOfClass:[NSObject class]]) {
+            NSLog(@"%@ is an instance of NSObject or one "
+                  "of its subclasses",
+                  [delorean model]);
+        } else {
+            NSLog(@"%@ is not an instance of NSObject or "
+                  "one of its subclasses",
+                  [delorean model]);
+        }
+        
+        // Check an object against a class, but not its subclasses
+        if ([delorean isMemberOfClass:[NSObject class]]) {
+            NSLog(@"%@ is a instance of NSObject",
+                  [delorean model]);
+        } else {
+            NSLog(@"%@ is not an instance of NSObject",
+                  [delorean model]);
+        }
+        
+        // Convert between strings and classes
+        if (NSClassFromString(@"Car") == [Car class]) {
+            NSLog(@"I can convert between strings and classes!");
+        }
+    
     }
     return 0;
 }
