b0c768fbea1927cd7175e74701e307352ce9fb3c
thodep <thodep@yahoo.com>
2015-04-09 21:37:46 -0400

    .

---
+++ Challenge6.h
@@ -0,0 +1,13 @@
+//
+//  Challenge6.h
+//  Car
+//
+//  Created by tho dang on 2015-04-09.
+//  Copyright (c) 2015 TD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Challenge6 : NSObject
+
+@end
---
+++ Challenge6.m
@@ -0,0 +1,16 @@
+//
+//  Challenge6.m
+//  Car
+//
+//  Created by tho dang on 2015-04-09.
+//  Copyright (c) 2015 TD. All rights reserved.
+//
+
+#import "Challenge6.h"
+
+// Define a class called XYPoint that will hold a Cartesian coordinate (x, y), where x and y are integers. Define methods to individually set the x and y coordinates of a point and retrieve their values. Write an Objective-C program to implement your new class and test it.
+
+
+@implementation Challenge6
+
+@end
